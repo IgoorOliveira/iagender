6f249ecc5b91e051ce5c22bfba5342b37ec09f34
Igor Oliveira <dev.igoroliveiraa@gmail.com>
2023-10-31 16:25:41 -0300

    feat: add progress bar and first part of multi step form

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.index, name=""),
+    path('register/', views.register, name="register")
 ]
---
+++ static/js/index.js
@@ -0,0 +1,81 @@
+const nextButton = document.querySelector(".next-button")
+const backButton = document.querySelector(".back-button")
+
+const formTitle = document.querySelector(".form-title");
+const formSubtitle = document.querySelector(".form-subtitle");
+let currentFormStep = 1;
+
+
+const textFormStep = {
+    1: {
+        "title": "Informações pessoais ",
+        "subtitle": "Comece por nos contar um pouco sobre você"
+    },
+    2: {
+        "title": "Informações cadastrais ",
+        "subtitle": "Protegendo a sua conta"
+    },
+    3: {
+        "title": "Informações cadastrais",
+        "subtitle": "Como podemos entrar em contato com você?"
+    },
+    4: {
+        "title": "Localização",
+        "subtitle": "Como podem encontrar você?"
+    },
+    5: {
+        "title": "Agenda",
+        "subtitle": "Escolha o horário que se adapta a sua rotina"
+    }
+}
+
+
+function showNextForm() {
+    document.querySelector(`.form-register-step${currentFormStep}`).classList.toggle("hidden");
+    document.querySelector(`.form-register-step${currentFormStep + 1}`).classList.toggle("hidden");
+}
+
+
+function showLastForm() {
+    document.querySelector(`.form-register-step${currentFormStep}`).classList.toggle("hidden");
+    document.querySelector(`.form-register-step${currentFormStep - 1}`).classList.toggle("hidden");
+}
+
+function showStepText() {
+    formTitle.innerText = textFormStep[currentFormStep]["title"];
+    formSubtitle.innerText = textFormStep[currentFormStep]["subtitle"];
+}
+
+function advanceProgressBar() {
+    document.querySelector(`.bar-progress-step${currentFormStep}`).classList.toggle("hidden");
+    document.querySelector(`.bar-progress-step${currentFormStep + 1}`).classList.toggle("hidden");
+    document.querySelector(`.bar-progress-step${currentFormStep }`).parentElement.classList.toggle('active-bar');
+    document.querySelector(`.bar-progress-step${currentFormStep + 1}`).parentElement.classList.toggle('active-bar');
+    document.querySelector(`.bar-icon-step${currentFormStep + 1}`).classList.add("fill-primary", "stroke-white");
+}
+
+function returnProgressBar() {
+    document.querySelector(`.bar-progress-step${currentFormStep}`).classList.toggle("hidden");
+    document.querySelector(`.bar-progress-step${currentFormStep - 1}`).classList.toggle("hidden");
+    document.querySelector(`.bar-progress-step${currentFormStep }`).parentElement.classList.toggle('active-bar');
+    document.querySelector(`.bar-progress-step${currentFormStep - 1}`).parentElement.classList.toggle('active-bar');
+    document.querySelector(`.bar-icon-step${currentFormStep}`).classList.remove("fill-primary", "stroke-white");
+}
+
+nextButton.addEventListener("click", ()=>{
+    showNextForm();
+    advanceProgressBar()
+    currentFormStep += 1;
+    showStepText();  
+})
+backButton.addEventListener("click", ()=>{
+    if(currentFormStep > 1) {
+        showLastForm();
+        returnProgressBar()
+        currentFormStep -= 1;
+        showStepText();
+    }
+    
+})
+
+
